565684c74290655cf0ff17efa22608c59fb1ea75
Rasogha <liyanagerasogha59@gmail.com>
2025-12-09 03:11:12 +0530

    initialized the back end repo and create authentication

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import userRouter from './routes/userRouter.js'
+import productRouter from './routes/productRouter.js'
+import jwt from 'jsonwebtoken'
+
+let app = express() 
+
+app.use(bodyParser.json()) 
+app.use((req,res,next)=>{
+    let token = req.header
+    ("Authorization")
+    
+    //remove "bearer "
+    if(token != null){
+        token = token.replace("Bearer ","")
+
+        jwt.verify(token,"your_jwt_secret_key",
+        (err,decoded)=>{
+            if(!err){
+                console.log("Decoded JWT:", decoded)
+                req.user = decoded
+            }
+        })
+    }next()
+})
+
+
+let mongoUrl = "mongodb+srv://admin:123@cluster0.pdpjoua.mongodb.net/?appName=Cluster0"
+
+mongoose.connect(mongoUrl)
+
+let connection = mongoose.connection
+
+connection.once('open', ()=>{
+    console.log("MongoDB database connection established successfully")
+})
+
+app.use('/api/users', userRouter)
+app.use('/api/products', productRouter)
+
+app.listen(3000,()=>{
+    console.log("Server started on port 3000");
+})
